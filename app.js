70db96e4e1994f14ec783b43e0f26e44844cb98b
bharath-kadiyala <kadiyalabharath8877@gmail.com>
2023-04-11 09:13:13 +0000

    commit gadgets page

---
+++ app.js
@@ -0,0 +1,8 @@
+const express = require("express");
+let app = express();
+
+app.get("/gadgets", (request, response) => {
+  response.sendFile("./gadgets.html", { root: __dirname });
+});
+
+module.exports = app;
